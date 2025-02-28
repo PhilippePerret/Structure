68c75da8746e0cf3886f176cc12587244ef0e06e
PhilippePerret <philippe.perret@yahoo.fr>
2025-02-28 03:25:28 +0100

    Traitement correct des horloges et des opérations temporelles

--- README.md
+++ README.md
@@ -1,5 +1,60 @@
 # Stt
 
-## Todo
+## Horloges
 
-* Passer les left et autres valeurs horizontales en "minutes" (leur donner le nom "time" et traiter *[time])
+On peut passer les temps sous de nombreux formats et, à la base, le format : 
+
+~~~
+H:MM:SS   p.e. 1:23:45
+~~~
+
+Par commodité, on peut remplacer les deux points par des virgules "," (plus facile à taper).
+
+~~~
+H,MM,SS p.e 1,23,45
+~~~
+
+On peut aussi fournir l'horloge par un calcul sur les secondes.
+
+~~~
+12 * 5 + 3600
+=> 1:01:00
+~~~
+
+On peut faire n'importe quelle opération d'ajout ou de retrait sur les horloges à l'aide de "+" ou de "-".
+
+~~~
+H:MM:SS + <valeur en secondes>
+~~~
+
+La valeur en secondes pour être un nombre :
+
+~~~
+1,23,45 + 12
+=> 1:23:57
+
+1,23,45 - 12
+=> 1:23:33
+
+~~~
+
+… une opération arythmétique :
+
+~~~
+1,23,45 + 4 * 3600 + 12
+=> 5:23:57
+~~~
+
+… une autre horloge :
+
+~~~
+1,23,45 + 4,0,12
+=> 5:23:57
+~~~
+
+… une expression avec "s" pour secondes, "m" pour minutes et "h" pour heures :
+
+~~~
+1,23,45 + 12s 4h
+=> 5:23:57
+~~~
--- assets/js/structure.js
+++ assets/js/structure.js
@@ -288,12 +288,22 @@ class TimeCalc {
     const h = parseInt(s / 3600)
     s = s - h * 3600
     let m = parseInt(s / 60)
-    m = (full == FULL && m < 10) ? `0${m}` : m;
+    m = ((full == FULL || h) && m < 10) ? `0${m}` : m;
     s = s - m * 60
     s = s < 10 ? `0${s}` : s;
     const res = [m, s]
-    if ( full == FULL) res.push(h) ;
+    if ( h || full == FULL) res.unshift(h) ;
     return res.join(":")
+  }
+
+  static get reghorl(){
+    return "([0-9]{1,4}(?:[:,][0-9]{1,2})?(?:[:,][0-9]{1,2})?)"
+  }
+  static get REG_HORLOGE(){
+    return this._reghor || (this._reghor = new RegExp(`^${this.reghorl}$`))
+  }
+  static get REG_HORLOGE_WITH_OPE(){
+    return this._regopeh || (this._regopeh = new RegExp(`^${this.reghorl}([+\-])(.+)$`))
   }
 
   /** 
@@ -301,25 +311,59 @@ class TimeCalc {
    */
   static treateAsOpeOnTime(timePlus, full = FULL){
     if ( timePlus === null ) return null;
-    timePlus = timePlus.trim()
-    if (ElementForm.IsATime(timePlus)) return TimeCalc.s2h(TimeCalc.h2s(timePlus), full);
-    timePlus = timePlus.replace(/ /g, "")
-    const [horloge, ajout] = timePlus.split("+")
-    console.info("horloge", horloge)
-    console.info("ajout", ajout)
-    const secondes = (horloge, ajout => {
-      if ( /([0-9]{1,2}([smh])){1,3}/.test(ajout) ) {
-        console.info("ajout avec lettre", ajout)
-      } else if ( ElementForm.IsATime(ajout) ) {
-        return this.h2s(horloge) + this.h2s(ajout)
+    timePlus = timePlus.trim().replace(/ /g, "")
+    let segs = timePlus.match(this.REG_HORLOGE_WITH_OPE)
+    if ( segs === null ) {
+      if ( timePlus.match(this.REG_HORLOGE) ) {
+        full = this.isRequiredFullHorloge(timePlus, full)
+        return TimeCalc.s2h(TimeCalc.h2s(timePlus), full);
       } else {
-        console.info("ajout par secondes", ajout)
-        return this.h2s(horloge) + parseInt(ajout)
+        // Sinon, on considère que c'est juste l'"ajout" qui est
+        // fourni, par exemple l'expression "12h 4m"
+        segs = [null, "0:00", "+", timePlus]
       }
-      })(horloge, ajout)
-    return this.s2h(secondes)
+    }
+    let [_tout, horloge, operation, ajout] = segs
+    // console.info("horloge", horloge)
+    // Si le format de départ de l'horloge est une horloge complète, 
+    // on doit retourner une horloge complète
+    full = this.isRequiredFullHorloge(horloge, full)
+    // Dans le cas où ajout n'est pas défini et que horloge n'est pas
+    // une horloge, c'est que c'est juste l'opération qui est fournie
+    if ( ajout == "" && ElementForm.NotATime(horloge) ) {
+      [horloge, ajout] = ["0:00", horloge]
+    }
+    const secondes = ((horloge, ajout, operation) => {
+      if ( /([0-9]{1,2}([smh])){1,3}/.test(ajout) ) {
+        ajout = [...ajout.matchAll(/[0-9]{1,3}[smh]/g)].map(res => {return res[0]}).join("+")
+        ajout = ajout.replace(/m/g,"*60").replace(/s/g,"").replace(/h/g,"*3600")
+        return this.makeOpe(this.h2s(horloge), eval(ajout), operation)
+      } else if ( ElementForm.IsATime(ajout) ) {
+        return this.makeOpe(this.h2s(horloge), this.h2s(ajout), operation)
+      } else {
+        return this.makeOpe(this.h2s(horloge), eval(ajout), operation)
+      }
+      })(horloge, ajout, operation)
+    return this.s2h(secondes, full)
   }
 
+  static makeOpe(term1, term2, ope){
+    if (ope == "+") {
+      return term1 + term2
+    } else {
+      return term1 - term2
+    }
+  }
+
+  // @return FULL s'il faut retourner une horloge complète ou false
+  // dans le cas contraire.
+  // On doit retourner une horloge complète (même s'il n'y a pas
+  // d'heures ou de minutes) si +full+ est FULL ou si l'horloge 
+  // fournie est complète.
+  static isRequiredFullHorloge(horloge, full){
+    if ( full == FULL ) return full ;
+    return horloge.split(/[,:]/).length == 3 ? FULL : false ;
+  }
 
 }
 
